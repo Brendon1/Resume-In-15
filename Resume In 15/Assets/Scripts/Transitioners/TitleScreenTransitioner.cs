ed3b4f47746d1bb189014c956192daa03f66bce2
egoulard-2000 <emilegoulard@gmail.com>
2022-07-22 21:25:07 -0600

    Finished TitleScreen

--- Resume In 15/Assets/Scripts/Transitioners/TitleScreenTransitioner.cs
+++ Resume In 15/Assets/Scripts/Transitioners/TitleScreenTransitioner.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.Video;
+using UnityEngine.UI;
 
 public class TitleScreenTransitioner : MonoBehaviour
 {
@@ -17,10 +19,19 @@ public class TitleScreenTransitioner : MonoBehaviour
     [SerializeField] private TextMeshProUGUI titleName;
     private float time = 15;
 
+    [SerializeField] private GameObject randomAd;
+    private Vector2 adSize = new Vector2(1920, 1080);
+    private List<Object> optionalList;
+    //private List<Object> adList;
+
     void Start()
     {
         //Setup title screen immediately
         titleName.text = "Resume In " + time;
+
+        // Read in all ad videos
+        optionalList = new List<Object>(Resources.LoadAll("OptionalVideos"));
+        //adList = new List<Object>(Resources.LoadAll("AdVideos"));
     }
 
     #region Animation Functions
@@ -28,6 +39,7 @@ public class TitleScreenTransitioner : MonoBehaviour
     {
         _animation.Play(start, 0, 0.0f);
         StartCoroutine(DelayTime(2));
+        ShowRandomAd(randomAd);
         StartCoroutine(Resume_In_15_Timer());
     }
 
@@ -55,7 +67,52 @@ public class TitleScreenTransitioner : MonoBehaviour
             time--;
         }
 
+        time = 0;
         EndTitleAnim();
     }
+    #endregion
+
+    #region Spawn Ads
+    public void ShowRandomAd(GameObject ad)
+    {
+        // Create new render texture
+        //var rendTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        var rendTexture = new RenderTexture((int)adSize.x, (int)adSize.y, 24);
+
+        // Add texture to ad obj raw image
+        ad.GetComponent<RawImage>().texture = rendTexture;
+
+        // Add texture to ad obj video player
+        ad.GetComponent<VideoPlayer>().targetTexture = rendTexture;
+
+        // Select random video from list
+        int index = Random.Range(0, optionalList.Count);
+
+        // add video clip to video player
+        ad.GetComponent<VideoPlayer>().clip = (VideoClip)optionalList[index];
+
+        ad.SetActive(true);
+    }
+
+    //public void ShowRandomSmallAd()
+    //{
+    //    // Create new render texture
+    //    //var rendTexture = new RenderTexture(Screen.width, Screen.height, 24);
+    //    var rendTexture = new RenderTexture((int)adSize.x, (int)adSize.y, 24);
+
+    //    // Add texture to ad obj raw image
+    //    randomSmallAd.GetComponent<RawImage>().texture = rendTexture;
+
+    //    // Add texture to ad obj video player
+    //    randomSmallAd.GetComponent<VideoPlayer>().targetTexture = rendTexture;
+
+    //    // Select random video from list
+    //    int index = Random.Range(0, adList.Count);
+
+    //    // add video clip to video player
+    //    randomSmallAd.GetComponent<VideoPlayer>().clip = (VideoClip)adList[index];
+
+    //    randomSmallAd.SetActive(true);
+    //}
     #endregion
 }
